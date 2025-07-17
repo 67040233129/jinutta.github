339fd44d9033fd288b1acab46f46784d39f9e9f2
67040233129 <67040233129@udru.ac.th>
2025-07-17 16:16:24 +0700

    labsql01.sql

---
+++ labsql01.sql
@@ -0,0 +1,6 @@
+SELECT* from products;
+SELECT* from Employees;
+SELECT EmployeeID, FirstName, LastName from Employees;
+SELEcT * from Employees where City =  'London';
+SELECT * from Categories;
+SELECT * from customers;
