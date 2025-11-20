a7aa3cf75e1dadeb3d1f1b536b276acccb83db5d
67040233129 <67040233129@udru.ac.th>
2025-11-20 09:45:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta chrset ="UTF -8>
+        <title>Document</title>
+</nead>
+<body>
+    < ?php
+    echo "Hello, world!";
+    echo "<br>";
+    echo"<h2>my Name is john Doe .</h2>";
+    ?>
+</body>
+</html>
